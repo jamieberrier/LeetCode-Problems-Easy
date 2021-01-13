73bc7619594b521924d686fc2dbbd2dbad384fd2
jamieberrier <berrier.jamie@gmail.com>
2021-01-13 12:13:40 -0800

    Update 485.MaxConsecutiveOnes.js

--- 485.MaxConsecutiveOnes.js
+++ 485.MaxConsecutiveOnes.js
@@ -1,3 +1,9 @@
+// Given a binary array, find the maximum number of consecutive 1s in this array.
+// Note:
+
+// The input array will only contain 0 and 1.
+// The length of input array is a positive integer and will not exceed 10,000
+
 /**
  * @param {number[]} nums
  * @return {number}
